4341d78bfe72b81ec351e204754cb3632ad263d2
Ali Zain Bukhari <alizainbukhari7860@gmail.com>
2022-01-09 15:33:02 +0500

    First push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      // className="App"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100%",
+      }}
+    >
+      <div>
+        <p
+          style={{ fontSize: "50px", fontWeight: "bold", textAlign: "center" }}
         >
-          Learn React
-        </a>
-      </header>
+          Clear Our Payments
+        </p>
+        <p
+          style={{ fontSize: "32px", fontWeight: "bold", textAlign: "center" }}
+        >
+          Mr Ahmed
+        </p>
+      </div>
     </div>
   );
 }
